9d6b4b965c32b6fd4957a3eb9a6a8edf84a38a17
Paloma Menendez Palau <palomamenendezpalau@Palomas-MacBook-Pro.local>
2024-11-21 15:54:44 -0300

    new version of calendly in request appointment working

--- app/(tabs)/request-appointment.jsx
+++ app/(tabs)/request-appointment.jsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Button, Linking } from 'react-native';
 
-const RequestAppointment = () => {
+const OpenCalendly = () => {
+  const openCalendly = () => {
+    Linking.openURL('https://calendly.com/luzzijuanma');
+  };
+
   return (
-    <View>
-      <Text>Solcitiar Turno</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 16 }}>Solicitar Turno</Text>
+      <Button title="Abrir Calendly" onPress={openCalendly} />
     </View>
-  )
-}
+  );
+};
 
-export default RequestAppointment
+export default OpenCalendly;
